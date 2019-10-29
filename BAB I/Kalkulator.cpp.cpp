78feb06948032218143b3f8763e2796298428b1c
novinurisnaeninovi571 <novinurisnaeni456@gmail.com>
2019-10-29 07:50:35 +0700

    dasar-pemograman

---
+++ BAB I/Kalkulator.cpp.cpp
@@ -0,0 +1,13 @@
+# include <iostream>
+using namespace std;
+
+int main()
+{
+    cout << " 2+2 = " << 2+2 << endl;
+    cout << " 3-2 = " << 3-2 << endl;
+    cout << " 4*2 = " << 4*2 << endl;
+    cout << " 5/2 = " << 5/2 << endl;
+
+    return 0;
+
+}
---
+++ BAB I/biodata novi.cpp
@@ -0,0 +1,14 @@
+# include <iostream>
+using namespace std;
+
+int main()
+{
+    cout << " Program Biodata! " << endl;
+    cout << " --------------------- " << endl;
+    cout << " Nama : Novi Nurisnaeni " << endl;
+    cout << " Umur : 19 " << endl;
+    cout << " Alamat : Sumedang " << endl;
+
+    return 0;
+
+}
